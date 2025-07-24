35e7a025b8bfa1276269954f2d12b3d9bcba7646
coldriver <17830831606@163.com>
2025-07-24 11:13:05 +0100

    feat: add gesture sensor module

---
+++ gesture_sensor.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from dotenv import load_dotenv
+from time import sleep
+
+load_dotenv()
+SENSOR_SDK_PATH = os.getenv("SENSOR_SDK_PATH")
+sys.path.append(SENSOR_SDK_PATH)
+
+from DFRobot_PAJ7620U2 import *
+paj = DFRobot_PAJ7620U2(1)
+
+
+class GestureSensor:
+    def __init__(self,callback):
+        self.callback = callback
+        while(paj.begin()!=0):
+            sleep(0.5)
+        paj.set_gesture_highrate(True)
+    
+    def start(self):
+        while True:
+            gesture = paj.get_gesture()
+            if gesture == paj.GESTURE_NONE:
+                continue
+            self.callback(paj.gesture_description(gesture))
+            sleep(0.1)
+
+if __name__ == "__main__":
+    sensor = GestureSensor(lambda x:print(x))
+    sensor.start()
